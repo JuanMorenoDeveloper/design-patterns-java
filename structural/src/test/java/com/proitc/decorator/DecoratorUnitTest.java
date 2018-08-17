834ed53bedab6b7bc7afdd1a18b93a72e09ba624
Juan Moreno <earth001@gmail.com>
2018-08-17 17:22:27 -0300

    Se agrega test adicional a decorator, links de presentación y fix en pom de sub modulos

--- structural/src/test/java/com/proitc/decorator/DecoratorUnitTest.java
+++ structural/src/test/java/com/proitc/decorator/DecoratorUnitTest.java
@@ -1,6 +1,9 @@
 package com.proitc.decorator;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,8 +11,8 @@ public class DecoratorUnitTest {
 
   @Test
   public void givenZeroNumber_whenDoJobValidatorZero_thenThrowRuntimeException() {
-    Component component = new ConcreteComponent();
-    AbstractDecorator validatorZero = new ConcreteDecoratorValidatorZero();
+    final Component component = new ConcreteComponent();
+    final AbstractDecorator validatorZero = new ConcreteDecoratorValidatorZero();
     validatorZero.setTheComponent(component);
 
     assertThatThrownBy(() -> validatorZero.doJob(0)).isInstanceOf(RuntimeException.class)
@@ -18,11 +21,23 @@ public class DecoratorUnitTest {
 
   @Test
   public void givenPositiveNumber_whenDoJobValidatorPositive_thenThrowRuntimeException() {
-    Component component = new ConcreteComponent();
-    AbstractDecorator validatorPositive = new ConcreteDecoratorValidatorPositive();
+    final Component component = new ConcreteComponent();
+    final AbstractDecorator validatorPositive = new ConcreteDecoratorValidatorPositive();
     validatorPositive.setTheComponent(component);
 
     assertThatThrownBy(() -> validatorPositive.doJob(1)).isInstanceOf(RuntimeException.class)
       .hasMessage("Number can't be positive");
+  }
+
+  @Test
+  public void givenNegativeNumber_whenDoJobValidatorPositive_thenCallDoJob() {
+    final Component component = new ConcreteComponent();
+    final Component spyComponent = spy(component);
+    final AbstractDecorator validatorPositive = new ConcreteDecoratorValidatorPositive();
+    validatorPositive.setTheComponent(spyComponent);
+    final int number = -1;
+    validatorPositive.doJob(number);
+
+    verify(spyComponent, times(1)).doJob(number);
   }
 }
